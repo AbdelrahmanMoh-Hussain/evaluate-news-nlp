460c150690f301b7909fc52cdadedba23069b330
Abdelrahman-Mohamed <abdelrahman.mohamed7337@gmail.com>
2024-11-11 14:40:18 +0200

    .

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,14 +1,94 @@
 // js files
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 // alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
+
+document
+  .getElementById("submitButton")
+  .addEventListener("click", preformAction);
+
+function preformAction() {
+  console.log("CLICKED");
+  const textareaContent = document.getElementById("name").value;
+  console.log(document.getElementById("name"));
+  console.log(textareaContent);
+
+  postDate("/add", { text: textareaContent }).then(function (flag) {
+    console.log(flag);
+    getData().then(function(data){
+      console.log(data);
+      analysisData(data).then(function(res) {
+        console.log(res)
+      })
+    })
+  });
+}
+
+const getData = async () => {
+  console.log("HERE");
+  const response = await fetch("/get");
+  try {
+    const res = await response.json();
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const postDate = async (url, data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const analysisData = async (data) => {
+  console.log("ANAALYSIS");
+  const requestOptions = {
+    method: "POST",
+    body: data,
+    redirect: "follow",
+  };
+  const response = await fetch(
+    "https://api.meaningcloud.com/sentiment-2.1",
+    requestOptions
+  );
+
+  try {
+    const res = await response.json();
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+//   .then(response => ({
+//     status: response.status,
+//     body: response.json()
+//   }))
+//   .then(({ status, body }) => console.log(status, body))
+//   .catch(error => console.log('error', error));
--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -1,12 +1,12 @@
-var path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,20 +14,35 @@ app.use(bodyParser.json());
 console.log(__dirname);
 
 // Variables for url and api key
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    res.sendFile('./dist/index.html')
+app.get("/", function (req, res) {
+  console.log("HERE GET")
+  res.sendFile("./dist/index.html");
 });
-
-
+const formdata = new FormData();
+const data = {}
+app.get('/get', (req, res) => {
+  console.log("HERE TO GET")
+  console.log(data)
+  res.send(data);
+});
 // POST Route
+app.post("/add", (req, res) => {
+  console.log("HERE POST")
+  console.log(req.body)
+  // formdata.append("key", process.env.API_KEY);
+  // formdata.append("txt", req.body.text);
+  // formdata.append("lang", "en");
+  data.key = process.env.API_KEY;
+  data.txt = req.body.text;
+  data.lang = "en";
 
-
+  console.log(formdata);
+  res.send({success: true});
+});
 
 // Designates what port the app will listen to for incoming requests
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
 });
-
-
